219262a13697e58f2e050ce2db0de11843402eda
Thomas Bibby <thomas@bibby.ie>
2016-03-23 11:19:47 +0000

    Initial Commit

---
+++ SleepLogger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SleepLogger
+//
+//  Created by Thomas Bibby on 23/03/2016.
+//  Copyright © 2016 Thomas Bibby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SleepLogger/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SleepLogger
+//
+//  Created by Thomas Bibby on 23/03/2016.
+//  Copyright © 2016 Thomas Bibby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
